65c475a45c5901f3060dc768d6e738d959420ed9
movesoft18 <movesoft@yandex.ru>
2024-11-19 10:48:01 +0400

    файлы

---
+++ streams/streams/streams.cpp
@@ -0,0 +1,14 @@
+﻿// streams.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string s;
+    getline(cin, s);
+    //cin >> s;
+    cout << s;
+}
