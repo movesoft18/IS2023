3b21f987d38f362fe0444cf6876f01eefd67a0bc
movesoft18 <movesoft@yandex.ru>
2025-03-25 17:38:21 +0400

    multimap set ranges

---
+++ Ranges/Ranges/Ranges.cpp
@@ -0,0 +1,49 @@
+﻿// Ranges.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <vector>
+#include <ranges>
+
+using namespace std;
+
+struct Person
+{
+    int id;
+    string name;
+    int age;
+};
+
+bool ageMoreThen30(const Person& p){return p.age > 30;}
+
+int main()
+{
+    vector<Person> person
+    {
+        Person{1, "Tom", 20},
+        Person{2, "Kate", 31},
+        Person{3, "Bob", 40},
+    };
+
+    //  вариант копирования
+ /*   vector<Person> person31;
+
+    for (auto& p : person)
+    {
+        if (p.age > 30)
+            person31.push_back(p);
+    }
+
+    for (auto& p : person31)
+        cout << p.id << ' ' << p.name << ' ' << p.age << endl;
+ */
+    auto view1 = person 
+        | views::filter(ageMoreThen30);
+    auto view2 = person
+        | views::filter([](auto& p) {return p.age > 30; });
+    for (const auto& v : view1)
+    {
+        cout << v.id << ' ' << v.name << ' ' << v.age << endl;
+    }
+
+}
---
+++ stl_containers/Set/Set/Set.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <set>
+
+using namespace std;
+
+template<typename T>
+void printSet(const set<T>& s)
+{
+    for (auto i : s)
+        cout << i << ' ';
+    cout << endl;
+}
+
+int main()
+{
+    set<int> numbers{1,4,7,4};
+    printSet(numbers);
+
+    cout << numbers.size() << endl;
+    cout << numbers.empty() << endl;
+    numbers.insert(10);
+    numbers.insert(20);
+    numbers.insert(30);
+    numbers.insert(10);
+    printSet(numbers);
+    numbers.erase(4);
+    printSet(numbers);
+    cout << (numbers.count(10)) << endl;
+}
