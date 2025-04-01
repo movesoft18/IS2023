741ced6aac2114ea78279a30affb6708ae5f80b6
movesoft18 <movesoft@yandex.ru>
2025-04-01 17:12:19 +0400

    ThreadExample

---
+++ Thread/ThreadExample/ThreadExample.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <thread>
+using namespace std;
+
+void threadFunc()
+{
+    cout << "Привет из потока с Id: "
+        << this_thread::get_id() << endl;
+    for (int i = 0; i < 2000; i++)
+        cout << this_thread::get_id() << " ";
+}
+
+int main()
+{
+    setlocale(LC_CTYPE, "");
+    thread thr1(threadFunc);
+    thread thr2(threadFunc);
+    cout << "Привет из главного потока\n";
+    for (int i = 0; i < 2000; i++)
+        cout << "Главный поток! ";
+    thr1.join();
+    thr2.join();
+    cout << "Все потоки завершены\n";
+}
