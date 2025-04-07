1fe7b891817205e9028d0273ebcb3ce2b4d2db3f
movesoft18 <movesoft@yandex.ru>
2025-04-07 13:01:20 +0400

    Добавлен пример чтения файла в строку std::string

--- stl_containers/Split/Split.cpp
+++ stl_containers/Split/Split.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <vector>
 #include <set>
+#include <string>
 using namespace std;
 
 //Эта функция должна       поделить строку на части.
@@ -57,12 +58,18 @@ vector<string> multisplit(const string& str, const set<char>& delimiters = {' '}
 
 int main()
 {
-    setlocale(LC_ALL, "");
-    string example = "Эта  функция должна       поделить, строку на части.";
-    auto result = split(example, ' ');
+    setlocale(LC_CTYPE, "");
+    string example = "192.56, -21.2, 17.08, 22.8, -0.01, 0.02, 33.2, 43.8, -12.1, 14.5";
+    auto result = split(example, ',');
     for (auto& s : result)
         cout << s << endl;
     cout << endl;
+    vector<double> numbers;
+    for (auto& s : result)
+    {
+        double n = stod(s);
+        numbers.push_back(n);
+    }
     example = "Эта  функция,должна       поделить, строку.на.части.";
     auto result1 = multisplit(example, set<char> {' ', '.',','});
     for (auto& s : result1)
---
+++ streams/ReadTextFileToString/ReadTextFileToString/ReadTextFileToString.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+
+string ReadAllText(const string& fileName)
+{
+    ifstream f(fileName, ios::binary | ios::ate);
+    size_t size = f.tellg();
+    string result(size, '\0');
+    f.seekg(0);
+    f.read(&result[0], size);
+    return result;
+}
+
+int main()
+{
+    string cppFile = ReadAllText("ReadTextFileToString.cpp");
+    cout << cppFile << endl;
+}
+
