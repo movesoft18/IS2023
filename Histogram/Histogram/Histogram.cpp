f477ed092c52e42e8d0ab6c3bba7515d28467c28
movesoft18 <movesoft@yandex.ru>
2025-04-21 13:35:03 +0400

    Histogram

---
+++ Histogram/Histogram/Histogram.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <map>
+using namespace std;
+
+void PrintHistogram(
+    const char histogramSymbol, 
+    const char symbol,
+    int maxLength,
+    int totalSymbolsCount,
+    int symbolsCount
+    )
+{
+    int length = (double)symbolsCount / totalSymbolsCount * maxLength;
+    cout << symbol << " - " << symbolsCount << '\t';
+    if (length > 0)
+        for (int i = 0; i < length; i++)
+            cout << histogramSymbol;
+    else
+        cout << (char)221;
+    cout << ' ' << (double)symbolsCount / totalSymbolsCount * 100<< " %";
+    cout << endl;
+}
+
+int main()
+{
+    int total = 100000;
+    map<char, int> m
+    {
+        {'a', 80000},
+        {'b', 3000},
+        {'c', 6000},
+        {'d', 1000},
+        {'e', 1200},
+    };
+    for (auto& e : m)
+        PrintHistogram(219, e.first, 50, 100000, e.second);
+
+  /* for (int i = 0; i < 255; i++)
+        cout << i << ' ' << (char)i << ' '; */
+}
---
+++ Thread/ThreadsInc/ThreadsInc.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <thread>
+#include <mutex>
+using namespace std;
+
+int counter = 0;
+mutex m;
+
+void Increment()
+{
+    for (int i = 0; i < 2000000; i++)
+    {
+        m.lock();
+        counter++;
+        m.unlock();
+    }
+        
+}
+
+int main()
+{
+    cout << "start value counter=" << counter << endl;
+    //Increment();
+    //Increment();
+    thread t1(Increment);
+    thread t2(Increment);
+    if (t1.joinable()) t1.join();
+    if (t2.joinable()) t2.join();
+    cout << "counter=" << counter << endl;
+}
+
