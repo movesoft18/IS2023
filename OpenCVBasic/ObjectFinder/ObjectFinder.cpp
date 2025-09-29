47951046af2339c7f49428f7fdaf67136d06b1c6
movesoft18 <movesoft@yandex.ru>
2025-09-29 13:40:32 +0400

    matchTemplate

--- OpenCVBasic/ObjectFinder/ObjectFinder.cpp
+++ OpenCVBasic/ObjectFinder/ObjectFinder.cpp
@@ -6,58 +6,54 @@
 using namespace std;
 using namespace cv;
 
-void FindObject(Mat* img, Mat* temp, int &x, int &y, double &probability)
+int main()
 {
+    
+    vector<int> coords;
+    double corr = 1;
+    int x, y;
     double minVal;
     double maxVal;
     Point minLoc;
     Point maxLoc;
-    Mat result;
-    matchTemplate(*img, *temp, result, TM_CCORR_NORMED);
-    //imshow("Карта вероятностей", result);
-    //waitKey();
-    minMaxLoc(result, &minVal, &maxVal, &minLoc, &maxLoc);
-    probability = maxVal;
-    x = maxLoc.x;
-    y = maxLoc.y;
-    rectangle(*img, maxLoc, Point(x + temp->cols, y + temp->rows), Scalar(255,255,255), -1);
-    return;
-
-}
-
-int main()
-{
-    /*
-    vector<int> coords;
-    double probability = 1;
-    int x, y;
     Mat img = imread("field1.jpg");
-    Mat result = imread("field1.jpg");
+    Mat copy = imread("field1.jpg");
     Mat temp = imread("template.jpg");
+    Mat mask = imread("mask.jpg");
+    Mat result;
     if (img.empty()  || temp.empty())
     {
         cout << "Can't read one of the images" << endl;
         return 1;
     }
+    matchTemplate(img, temp, result, TM_CCORR_NORMED);
 
-    while (probability >= 0.87)
+    while (corr >= 0.87)
     {
-        FindObject(&img, &temp, x, y, probability);
-        if (probability >= 0.87)
+        minMaxLoc(result, &minVal, &corr, &minLoc, &maxLoc);
+        x = maxLoc.x;
+        y = maxLoc.y;
+       // result.at<float>(maxLoc) = 0.f;
+        if (corr >= 0.87)
         {
+            circle(result, Point(maxLoc), temp.cols / 2,
+                Scalar(0, 0, 0), -1);
             coords.push_back(x);
             coords.push_back(y);
-            ellipse(result, Point(x + temp.rows / 2, y + temp.cols / 2), Size(temp.rows / 2, temp.cols / 2), 0, 0, 360, Scalar(255, 0, 255), 2);
+            rectangle(copy, Rect(x, y, temp.cols, temp.rows), Scalar(255, 0, 255), 2);
+            //ellipse(result, Point(x + temp.rows / 2, y + temp.cols / 2), Size(temp.cols / 2, temp.rows / 2 ), 0, 0, 360, Scalar(255, 0, 255), 2);
         }
+        imshow("corr", result);
         imwrite("result.jpg", result);
-        imshow("Result", result);
+        imshow("Result", copy);
+        waitKey();
     }
     for (auto i = 0; i < coords.size()/2; i++)
     {
         cout << coords[i * 2] << ' ' << coords[i * 2 + 1] << endl;
     }
-    */
-    Mat result;
+    
+   /* Mat result;
     double minVal;
     double maxVal;
     Point minLoc;
@@ -67,13 +63,15 @@ int main()
     matchTemplate(img, temp, result, TM_CCORR_NORMED);
     imshow("results", result);
     minMaxLoc(result, &minVal, &maxVal, &minLoc, &maxLoc);
-    double probability = maxVal;
+    double corr = maxVal;
     int x = maxLoc.x;
     int y = maxLoc.y;
     ellipse(img, Point(x + temp.rows / 2, y + temp.cols / 2), 
-        Size(temp.rows / 2, temp.cols / 2), 0, 0, 360, Scalar(255, 0, 255), 2);
+        Size(temp.rows / 2, temp.cols / 2), 0, 0, 
+        360, Scalar(255, 0, 255), 2);
     imshow("Image", img);
+    cout << "x=" << x << " y=" << y << " corr=" << corr << endl;
     waitKey(0);
-
+    */
 }
 
